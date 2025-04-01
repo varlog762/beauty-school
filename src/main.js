0fa636a2ac8ed8af25b58c02b9363bf92d8a1c09
Gregory Pereverzev <boot762@gmail.com>
2025-04-01 20:30:54 +0300

    add prettier config

--- src/main.js
+++ src/main.js
@@ -1,41 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.forms["registration-form"];
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.forms['registration-form'];
   const formData = new FormData(form);
 
-  const button = document.getElementById("course-button");
-  const menu = document.getElementById("dropdown-menu");
-  const selectedText = document.getElementById("selected-course");
-  const select = document.getElementById("course-select");
+  const button = document.getElementById('course-button');
+  const menu = document.getElementById('dropdown-menu');
+  const selectedText = document.getElementById('selected-course');
+  const select = document.getElementById('course-select');
 
-  const nameInput = form["name-input"];
-  const phoneInput = form["phone-input"];
-  const submitButton = form["submit-button"];
+  const nameInput = form['name-input'];
+  const phoneInput = form['phone-input'];
+  const submitButton = form['submit-button'];
 
   console.log(submitButton);
 
   // Закрытие списка при клике вне его
-  document.addEventListener("click", (event) => {
+  document.addEventListener('click', event => {
     if (!button.contains(event.target) && !menu.contains(event.target)) {
-      menu.classList.add("hidden");
+      menu.classList.add('hidden');
     }
   });
 
   // Открытие/закрытие списка
-  button.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+  button.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
   });
 
   // Обработчик выбора элемента
-  menu.querySelectorAll("li").forEach((item) => {
-    item.addEventListener("click", () => {
+  menu.querySelectorAll('li').forEach(item => {
+    item.addEventListener('click', () => {
       const value = item.dataset.value;
       selectedText.textContent = item.textContent;
       select.value = value;
-      menu.classList.add("hidden");
+      menu.classList.add('hidden');
     });
   });
 
-  form.addEventListener("submit", (event) => {
+  form.addEventListener('submit', event => {
     event.preventDefault();
     console.dir(formData.entries());
   });
